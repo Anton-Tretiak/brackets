f55e1ccd2dd739d62b9e5006cae82e05e5a3d0db
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-07-30 00:19:54 +0300

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = str;
+  let len = str.length / 2;
+  for (let i = 0; i < len; i++) {
+    bracketsConfig.forEach(item => {
+      res = res.replace(item.join(''), '')
+    })
+  }
+  if (res.length === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
